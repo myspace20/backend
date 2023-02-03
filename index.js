c350c956e4b2de226048cc3cd1d0a07a10619c4a
Roger Satsi <rdsatsi001@st.ug.edu.gh>
2023-02-03 22:52:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type': 'text/plain'})
+//     res.end('Hello')
+
+//     console.log(req.url)
+// })
+
+
+// server.listen(3000, 'localhost', ()=>{
+//     console.log("server is running")
+// })
+
+//The Global Object
+
+// console.log(global)
+
+// global.setTimeout(()=>{
+//     console.log("Time is Up")
+//     clearInterval(every)
+// },3000)
+
+// const every = global.setInterval(()=>{
+//     console.log("I will run every second")
+// },1000)
+
+
+//Modules
+
+
+const { people, ages, totalMem, cpusData, userInfo } = require('./modules')
+
+
+console.log(people, ages, totalMem, cpusData, userInfo)
+
+
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type': 'text/plain'})
+    res.end('Hello')
+
+    console.log(req.url)
+})
+
+
+server.listen(3000, 'localhost', ()=>{
+    console.log("server is running")
+})
+
+
+
+
---
+++ modules.js
@@ -0,0 +1,37 @@
+const people = ['shaun', 'mario', 'luigi']
+const ages = [1,2,3,41,5,6]
+
+
+//Os module
+
+const os = require('os')
+
+
+const cpus = os.cpus()
+
+const userInfo = os.userInfo()
+
+const totalMem = os.totalmem()
+
+console.log("UserInfo :", userInfo)
+console.log("Total Mem :", totalMem)
+
+let cpusData = []
+
+cpus.forEach(cpu=>{
+    console.log(cpu.model, cpu.speed)
+    cpusData.push({model:cpu.model, speed:cpu.speed})
+})
+
+//File system
+
+
+// console.log(people)
+
+module.exports = {
+    people,
+    ages,
+    totalMem,
+    userInfo,
+    cpusData
+}
