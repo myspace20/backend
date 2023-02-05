53abec272d8f93f158e81d1a8d7864619aa02806
Roger Satsi <rdsatsi001@st.ug.edu.gh>
2023-02-05 14:04:46 +0000

    fs module practice

--- file.js
+++ file.js
@@ -1,4 +1,7 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile,existsSync, mkdir, unlink, open } = require('fs')
+
+const data = "Welcome to My Backend Practice Repo \nIt contains data on my progress as a Junior - Mid level Software Developer\nDay 1: Created my firsr Server using the http module\nDay 2: Learnt how to export and import modules in NodeJs\nDay 3: Learnt how to read and write to a file using the fs module"
+
 
 const readMyfile = (file) => {
     readFile(file, 'utf8', (err, data) => {
@@ -17,7 +20,38 @@ const writeToFile = (file, data) => {
 }
 
 
-const data = "Welcome to My Backend Practice Repo \nIt contains data on my progress as a Junior - Mid level Software Developer\nDay 1: Created my firsr Server using the http module\nDay 2: Learnt how to export and import modules in NodeJs\nDay 3: Learnt how to read and write to a file using the fs module"
+if(!existsSync('./assets')){
+    mkdir('./assets', (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("Successfully Created")
+    })
+}else{
+    throw new Error("Folder already exists")
+}
+
+if(!existsSync('sample.txt')){
+    open('sample.txt', 'w', (err, file)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log('file created successfully :'+ file)
+    })
+}else{
+    throw new Error("File already exists")
+}
+
+if(existsSync('sample.txt')){
+    unlink('sample.txt', (err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log('Successfully deleted')
+    })
+}
+
+
 
 writeToFile('README.md', data)
 // console.log(readMyfile('./text.txt'))
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 
-// const http = require('http')
+const http = require('http')
 
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-Type': 'text/plain'})
-//     res.end('Hello')
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type': 'text/plain'})
+    res.end('Hello')
 
-//     console.log(req.url)
-// })
+    console.log(req.url)
+})
 
 
-// server.listen(3000, 'localhost', ()=>{
-//     console.log("server is running")
-// })
+server.listen(3000, 'localhost', ()=>{
+    console.log("server is running")
+})
 
 //The Global Object
 
-// console.log(global)
+console.log(global)
 
-// global.setTimeout(()=>{
-//     console.log("Time is Up")
-//     clearInterval(every)
-// },3000)
+global.setTimeout(()=>{
+    console.log("Time is Up")
+    clearInterval(every)
+},3000)
 
-// const every = global.setInterval(()=>{
-//     console.log("I will run every second")
-// },1000)
+const every = global.setInterval(()=>{
+    console.log("I will run every second")
+},1000)
 
 
 //Modules
@@ -36,19 +36,9 @@ const { people, ages, totalMem, cpusData, userInfo } = require('./modules')
 console.log(people, ages, totalMem, cpusData, userInfo)
 
 
-// const http = require('http')
-
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-Type': 'text/plain'})
-//     res.end('Hello')
-
-//     console.log(req.url)
-// })
-
-
-// server.listen(3000, 'localhost', ()=>{
-//     console.log("server is running")
-// })
+server.listen(3000, 'localhost', ()=>{
+    console.log("server is running")
+})
 
 
 
